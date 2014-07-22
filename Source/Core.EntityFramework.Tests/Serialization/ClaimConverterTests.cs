d549241e7dd4307ade1b019c2f0fa1fd816b7793
Brock Allen <brockallen@gmail.com>
2014-07-22 10:07:38 -0400

    add test for claim converter

---
+++ Source/Core.EntityFramework.Tests/Serialization/ClaimConverterTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Claims;
+using Thinktecture.IdentityServer.Core;
+using Newtonsoft.Json;
+using Thinktecture.IdentityServer.Core.Models;
+using Thinktecture.IdentityServer.Core.EntityFramework.Serialization;
+
+namespace Thinktecture.IdentityServer.v3.EntityFramework.Tests.Serialization
+{
+    [TestClass]
+    public class ClaimConverterTests
+    {
+        [TestMethod]
+        public void CanSerializeAndDeserializeAClaim()
+        {
+            var claim = new Claim(Constants.ClaimTypes.Subject, "alice");
+
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(new ClaimConverter());
+            var json = JsonConvert.SerializeObject(claim, settings);
+
+            claim = JsonConvert.DeserializeObject<Claim>(json, settings);
+            Assert.AreEqual(Constants.ClaimTypes.Subject, claim.Type);
+            Assert.AreEqual("alice", claim.Value);
+        }
+    }
+}
