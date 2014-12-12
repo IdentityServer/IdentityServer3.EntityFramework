110949c2212b5a7196e69c3d0dd0dc9937b423e8
Dominick Baier <dbaier@leastprivilege.com>
2014-12-12 08:24:05 +0100

    updated build

--- build.ps1
+++ build.ps1
@@ -2,11 +2,17 @@ Param(
 	[string]$buildNumber = "0",
 	[string]$preRelease = $null
 )
-Write-Host 'prerelease:' $preRelease
+
+if(Test-Path Env:\APPVEYOR_BUILD_NUMBER) {
+	$d = Get-Date
+	$preRelease = "pre-" + [int]$Env:APPVEYOR_BUILD_NUMBER
+	Write-Host "Using APPVEYOR_BUILD_NUMBER"
+}
+
 gci .\source -Recurse "packages.config" |% {
 	"Restoring " + $_.FullName
-	.\source\.nuget\nuget.exe i $_.FullName -o .\packages
+	.\source\.nuget\nuget.exe i $_.FullName -o .\source\packages
 }
-Import-Module .\packages\psake.4.3.2\tools\psake.psm1
+Import-Module .\source\packages\psake.4.3.2\tools\psake.psm1
 Invoke-Psake .\default.ps1 $task -framework "4.0x64" -properties @{ buildNumber=$buildNumber; preRelease=$preRelease }
 Remove-Module psake
