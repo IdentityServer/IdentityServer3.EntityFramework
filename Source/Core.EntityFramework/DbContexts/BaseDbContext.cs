e8a0147fff525c4a7b7eaea8188a6bdeae2f5596
Brock Allen <brockallen@gmail.com>
2015-01-20 12:16:52 -0500

    report validation errors in SaveChangesAsync

--- Source/Core.EntityFramework/DbContexts/BaseDbContext.cs
+++ Source/Core.EntityFramework/DbContexts/BaseDbContext.cs
@@ -58,5 +58,29 @@ namespace Thinktecture.IdentityServer.Core.EntityFramework
                 throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
             }
         }
+
+        public override System.Threading.Tasks.Task<int> SaveChangesAsync()
+        {
+            try
+            {
+                return base.SaveChangesAsync();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                // Retrieve the error messages as a list of strings.
+                var errorMessages = ex.EntityValidationErrors
+                .SelectMany(x => x.ValidationErrors)
+                .Select(x => x.ErrorMessage);
+
+                // Join the list to a single string.
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                // Combine the original exception message with the new one.
+                var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                // Throw a new DbEntityValidationException with the improved exception message.
+                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+            }
+        }
     }
 }
