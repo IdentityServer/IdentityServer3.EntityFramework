a73fc33f3cfcb52b56ad2e9ef2fe41b829612731
Brock Allen <brockallen@gmail.com>
2015-01-23 14:29:54 -0500

    update build scripts

--- default.ps1
+++ default.ps1
@@ -1,10 +1,12 @@
 properties {
 	$base_directory = Resolve-Path . 
 	$src_directory = "$base_directory\source"
+	$output_directory = "$base_directory\build"
 	$dist_directory = "$base_directory\distribution"
 	$sln_file = "$src_directory\Thinktecture.IdentityServer.v3.EntityFramework.sln"
 	$target_config = "Release"
 	$framework_version = "v4.5"
+	$xunit_path = "$src_directory\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
 	$nuget_path = "$src_directory\.nuget\nuget.exe"
 	
 	$buildNumber = 0;
@@ -13,17 +15,16 @@ properties {
 }
 
 task default -depends Clean, CreateNuGetPackage
+task appVeyor -depends Clean, CreateNuGetPackage
 
 task Clean {
+	rmdir $output_directory -ea SilentlyContinue -recurse
 	rmdir $dist_directory -ea SilentlyContinue -recurse
 	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /t:Clean }
 }
 
 task Compile -depends UpdateVersion {
 	exec { msbuild /nologo /verbosity:q $sln_file /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.5 }
-
-	$versionAssemblyInfoFile = "$src_directory/VersionAssemblyInfo.cs"
-	rm $versionAssemblyInfoFile
 }
 
 task UpdateVersion {
@@ -44,6 +45,12 @@ task UpdateVersion {
 	"[assembly: AssemblyFileVersion(""$assemblyFileVersion"")]" >> $versionAssemblyInfoFile
 }
 
+task RunTests -depends Compile {
+	$project = "Thinktecture.IdentityServer.v3.EntityFramework.Tests"
+	mkdir $output_directory\xunit\$project -ea SilentlyContinue
+	.$xunit_path "$output_directory\$project.dll" /html "$output_directory\xunit\$project\index.html"
+}
+
 task CreateNuGetPackage -depends Compile {
 	$vSplit = $version.Split('.')
 	if($vSplit.Length -ne 4)
@@ -58,6 +65,11 @@ task CreateNuGetPackage -depends Compile {
 		$packageVersion = "$packageVersion-$preRelease" 
 	}
 
-	md $dist_directory
+	if ($buildNumber -ne 0){
+		$packageVersion = $packageVersion + "-build" + $buildNumber.ToString().PadLeft(5,'0')
+	}
+
+  md $dist_directory
+
 	exec { . $nuget_path pack $src_directory\Core.EntityFramework\Core.EntityFramework.csproj -o $dist_directory -version $packageVersion }
 }
