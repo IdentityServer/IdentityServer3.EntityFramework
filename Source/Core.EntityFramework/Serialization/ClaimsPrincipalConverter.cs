d5ab5337e2cb9a6e7492152f676b66800d6ebac9
Ken Dale <ken@kendaleiv.com>
2015-05-27 08:16:05 -0400

    Add using statement

--- Source/Core.EntityFramework/Serialization/ClaimsPrincipalConverter.cs
+++ Source/Core.EntityFramework/Serialization/ClaimsPrincipalConverter.cs
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+using IdentityServer3.Core;
 using Newtonsoft.Json;
 using System;
 using System.Linq;
